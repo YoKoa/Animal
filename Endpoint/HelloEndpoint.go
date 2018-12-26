f42b2c764c3b174854fab045ce721795cf3304b5
Zhang.Zhen <zhangzhen1213@thundersoft.com>
2018-12-26 16:36:05 +0800

    v0.2:增加限流、熔断和zipkin

--- Endpoint/HelloEndpoint.go
+++ Endpoint/HelloEndpoint.go
@@ -2,9 +2,29 @@ package Endpoint
 
 import (
 	"context"
+	"github.com/go-kit/kit/circuitbreaker"
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/ratelimit"
+	"github.com/go-kit/kit/tracing/zipkin"
+	"github.com/sony/gobreaker"
+	"golang.org/x/time/rate"
+	gozipkin "github.com/openzipkin/zipkin-go"
 	pb "google.golang.org/grpc/examples/helloworld/Proto"
 )
+
+func Hellopoint(limit *rate.Limiter,zipkinTracer *gozipkin.Tracer) endpoint.Endpoint{
+	var HelloPoint endpoint.Endpoint
+	{
+		HelloPoint = HelloEndpoint()
+		//rate.NewLimiter(rate.Every(time.Second), 1)
+		HelloPoint = ratelimit.NewErroringLimiter(limit)(HelloPoint)
+		HelloPoint = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))(HelloPoint)
+		//HelloPoint = opentracing.TraceServer()(HelloPoint)
+		HelloPoint = zipkin.TraceEndpoint(zipkinTracer,"hello")(HelloPoint)
+	}
+
+	return HelloPoint
+}
 
 func HelloEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
--- Transport/main.go
+++ Transport/main.go
@@ -5,23 +5,69 @@
 package main
 
 import (
+	"Animal/Endpoint"
 	"fmt"
 	grpc_transport "github.com/go-kit/kit/transport/grpc"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/examples/helloworld/Endpoint"
-	"google.golang.org/grpc/examples/helloworld/Service"
 	pb "google.golang.org/grpc/examples/helloworld/Proto"
+	"google.golang.org/grpc/examples/helloworld/Service"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"os"
+	"time"
 )
 
 const (
 	port = ":50051"
+	endpointURL = "http://localhost:9411/api/v2/spans"
 )
+//var fs = flag.NewFlagSet("hello", flag.ExitOnError)
+//var (
+//	//debugAddr      = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
+//	//httpAddr       = fs.String("http-addr", ":8081", "HTTP listen address")
+//	//grpcAddr       = fs.String("grpc-addr", ":8082", "gRPC listen address")
+//	//thriftAddr     = fs.String("thrift-addr", ":8083", "Thrift listen address")
+//	//jsonRPCAddr    = fs.String("jsonrpc-addr", ":8084", "JSON RPC listen address")
+//	//thriftProtocol = fs.String("thrift-protocol", "binary", "binary, compact, json, simplejson")
+//	//thriftBuffer   = fs.Int("thrift-buffer", 0, "0 for unbuffered")
+//	//thriftFramed   = fs.Bool("thrift-framed", false, "true to enable framing")
+//	zipkinV2URL    = fs.String("http://localhost:9411/api/v2/spans", "", "Enable Zipkin v2 tracing (zipkin-go) using a Reporter URL e.g. http://localhost:9411/api/v2/spans")
+//	//zipkinV1URL    = fs.String("http://localhost:9411/api/v1/spans", "", "Enable Zipkin v1 tracing (zipkin-go-opentracing) using a collector URL e.g. http://localhost:9411/api/v1/spans")
+//	//lightstepToken = fs.String("lightstep-token", "", "Enable LightStep tracing via a LightStep access token")
+//	//appdashAddr    = fs.String("appdash-addr", "", "Enable Appdash tracing via an Appdash server host:port")
+//)
+var limit = rate.NewLimiter(rate.Every(time.Second), 1)
+
 
 
 func main() {
+
+	var zipkinTracer *zipkin.Tracer
+	{
+		var(
+			err  error
+			hostPort= "localhost:0"
+			serviceName= "hello"
+			reporter= zipkinhttp.NewReporter(endpointURL)
+		)
+		defer reporter.Close()
+		sampler, err := zipkin.NewCountingSampler(1)
+		zEP, _ := zipkin.NewEndpoint(serviceName, hostPort)
+		zipkinTracer, err = zipkin.NewTracer(
+			reporter,
+			zipkin.WithSampler(sampler),
+			zipkin.WithLocalEndpoint(zEP),
+		)
+		if err != nil {
+			log.Fatal("err", err)
+			os.Exit(1)
+		}
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -29,7 +75,7 @@ func main() {
 
 	helloServer := new(Service.Server)
 	SayHelloHandler := grpc_transport.NewServer(
-		Endpoint.HelloEndpoint(),
+		Endpoint.Hellopoint(limit,zipkinTracer),
 		Endpoint.DecodeRequest,
 		Endpoint.EncodeResponse,
 	)
